d7c348f37f38dc579d7eb81d56ac2eeb11a23423
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-09-08 21:35:50 +0200

    Also do jest this way

--- build.js
+++ build.js
@@ -32,3 +32,36 @@ build({
   outfile: pkg.module, // "dist/hostic-dom.esm.js",
   format: 'esm',
 })
+
+// const fs = require('fs')
+// const esbuild = require('esbuild')
+//
+// const external = [
+//   ...Object.keys(pkg.dependencies ?? {}),
+//   ...Object.keys(pkg.devDependencies ?? {}),
+//   ...Object.keys(pkg.peerDependencies ?? {}),
+// ]
+//
+// module.exports = {
+//
+//   // https://jestjs.io/docs/en/troubleshooting#caching-issues
+//   getCacheKey() {
+//     return Math.random().toString()
+//   },
+//
+//   process(content, filename) {
+//     let file = `.hostic/${Math.random()}.js`
+//
+//     esbuild.buildSync({
+//       ...common,
+//       outfile: file,
+//       entryPoints: [filename],
+//       external,
+//     })
+//
+//     let js = fs.readFileSync(file, 'utf-8')
+//     fs.unlinkSync(file)
+//
+//     return js
+//   },
+// }
---
+++ jest-transformer.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const esbuild = require('esbuild')
+const pkg = require('./package.json')
+
+const common = {
+  bundle: true,
+  sitemap: true,
+  minify: true,
+  target: 'es2017',
+  loader: {
+    '.js': 'jsx',
+  },
+  jsxFactory: 'h',
+  external: [
+    'htmlparser2',
+    'css-what',
+  ],
+}
+
+const external = [
+  ...Object.keys(pkg.dependencies ?? {}),
+  ...Object.keys(pkg.devDependencies ?? {}),
+  ...Object.keys(pkg.peerDependencies ?? {}),
+]
+
+module.exports = {
+
+  // https://jestjs.io/docs/en/troubleshooting#caching-issues
+  getCacheKey() {
+    return Math.random().toString()
+  },
+
+  process(content, filename) {
+    let file = `.hostic/${Math.random()}.js`
+
+    esbuild.buildSync({
+      ...common,
+      outfile: file,
+      entryPoints: [filename],
+      external,
+    })
+
+    let js = fs.readFileSync(file, 'utf-8')
+    fs.unlinkSync(file)
+
+    return js
+  },
+}
